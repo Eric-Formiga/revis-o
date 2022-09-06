804882314458498e884c008108283415908f1e1b
Eric-Formiga <eric.13formiga@gmail.com>
2022-09-05 21:42:20 -0300

    commit inicial revisão

---
+++ js/ExerciosDiferencaDeIdades/DiferencaIdades.js
@@ -0,0 +1,9 @@
+var nomePVelha = prompt("Qual o nome da pessoa mais velha?")
+var idadePVelha = prompt("Qual a sua idade?")
+var nomePNova = prompt("Qual o nome da pessoa mais nova?")
+var idadePNova = prompt("Qual a sua idade?")
+var DifIdade = idadePVelha - idadePNova
+
+alert("O nome da pessoa mais velha é " + nomePVelha + " e sua idade é " + idadePVelha + " anos")
+alert("O nome da pessoa mais nova é " + nomePNova + " e sua idade é " + idadePNova + " anos")
+alert("A difernça de idade entre vocês é " + DifIdade + " anos")
