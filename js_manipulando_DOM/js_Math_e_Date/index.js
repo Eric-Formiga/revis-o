16b0d2a75545d765c8647e999174a598c5ee30ca
Eric-Formiga <eric.13formiga@gmail.com>
2022-09-17 21:44:17 -0300

    exercicios adicionados

---
+++ js_manipulando_DOM/js_Math_e_Date/index.js
@@ -0,0 +1,7 @@
+console.log(Math.pow(5,3))
+console.log(Math.PI)
+console.log(Math.sqrt(64))
+let  myBirthday = new Date(2001, 01,20)
+console.log(myBirthday)
+let today = Date.now()
+console.log(today)
---
+++ js_manipulando_DOM/seletores/index.js
@@ -0,0 +1,18 @@
+// function show(){
+//     let element = document.getElementById("name")
+//     console.log(element)
+// }
+// function show(){
+//     let element = document.getElementsByName("phone")
+//     console.log(element)
+// }
+
+// function show(){
+//     let element = document.querySelectorAll("div#phones input[name='phone']")
+//     console.log(element)
+// }
+
+function show(){
+    let element = document.getElementByTagName("input")
+    console.log(element)
+}
